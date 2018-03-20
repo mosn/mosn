ba619464eb940a645924363bc5d9f86a7f47a352
xiaodong.dxd <xiaodong.dxd@alibaba-inc.com>
2018-03-20 20:28:51 +0800

    code cleanu

--- pkg/stream/http2/stream.go
+++ pkg/stream/http2/stream.go
@@ -379,3 +379,16 @@ func decodeHeader(in map[string][]string) (out map[string]string) {
 
 	return
 }
+
+// io.ReadCloser
+type IoBufferReadCloser struct {
+	buf types.IoBuffer
+}
+
+func (rc *IoBufferReadCloser) Read(p []byte) (n int, err error) {
+	return rc.buf.Read(p)
+}
+
+func (rc *IoBufferReadCloser) Close() error {
+	return nil
+}
