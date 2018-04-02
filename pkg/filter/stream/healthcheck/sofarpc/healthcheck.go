8069601d5d783b91ff9eaa7ab30b7c39d63b072d
wugou.cyf <wugou.cyf@alipay.com>
2018-04-02 19:45:40 +0800

    fill heartbeat ack command properties(tr part wait for branch merge)

--- pkg/filter/stream/healthcheck/sofarpc/healthcheck.go
+++ pkg/filter/stream/healthcheck/sofarpc/healthcheck.go
@@ -6,6 +6,8 @@ import (
 	"gitlab.alipay-inc.com/afe/mosn/pkg/protocol/sofarpc"
 	"gitlab.alipay-inc.com/afe/mosn/pkg/api/v2"
 	"time"
+	"gitlab.alipay-inc.com/afe/mosn/pkg/log"
+	"gitlab.alipay-inc.com/afe/mosn/pkg/protocol/sofarpc/codec"
 )
 
 // todo: support cached pass through
@@ -19,6 +21,7 @@ type healthCheckFilter struct {
 	// request properties
 	intercept      bool
 	protocol       byte
+	requestId      uint32
 	healthCheckReq bool
 	// callbacks
 	cb types.StreamDecoderFilterCallbacks
@@ -35,10 +38,12 @@ func NewHealthCheckFilter(config v2.HealthCheckFilter) *healthCheckFilter {
 func (f *healthCheckFilter) DecodeHeaders(headers map[string]string, endStream bool) types.FilterHeadersStatus {
 	if cmdCodeStr, ok := headers[sofarpc.SofaPropertyHeader("cmdcode")]; ok {
 		cmdCode := sofarpc.ConvertPropertyValue(cmdCodeStr, reflect.Int16)
-
+		//sofarpc.HEARTBEAT(0) is equal to sofarpc.TR_HEARTBEAT(0)
 		if cmdCode == 0 {
 			protocolStr := headers[sofarpc.SofaPropertyHeader("protocol")]
 			f.protocol = sofarpc.ConvertPropertyValue(protocolStr, reflect.Uint8).(byte)
+			requestIdStr := headers[sofarpc.SofaPropertyHeader("requestid")]
+			f.requestId = sofarpc.ConvertPropertyValue(requestIdStr, reflect.Uint32).(uint32)
 			f.healthCheckReq = true
 			f.cb.RequestInfo().SetHealthCheck(true)
 
@@ -86,14 +91,42 @@ func (f *healthCheckFilter) DecodeTrailers(trailers map[string]string) types.Fil
 func (f *healthCheckFilter) handleIntercept() {
 	// todo: cal status based on cluster healthy host stats and f.clusterMinHealthyPercentages
 
-	resp := &sofarpc.BoltResponseCommand{
-		Protocol: f.protocol,
+	var resp interface{}
+
+	switch {
+	//case f.protocol == sofarpc.PROTOCOL_CODE:
+	//	resp = newTrHeartbeatAck()
+	case f.protocol == sofarpc.PROTOCOL_CODE_V1 || f.protocol == sofarpc.PROTOCOL_CODE_V2:
+		//boltv1 and boltv2 use same heartbeat struct as BoltV1
+		resp = newBoltHeartbeatAck(f.protocol, f.requestId)
+	default:
+		log.DefaultLogger.Debugf("Unknown protocol code: [", f.protocol, "] while intercept healthcheck.")
+	}
+
+	f.cb.EncodeHeaders(resp, true)
+}
+
+/**
+func newTrHeartbeatAck() *sofarpc.TrResponseCommand{
+	return &sofarpc.TrResponseCommand{
+		Protocol: sofarpc.PROTOCOL_CODE,
 		CmdType:  byte(2),
 		CmdCode:  int16(0),
 		// todo: fill in more properties...
 	}
+}
+**/
 
-	f.cb.EncodeHeaders(resp, true)
+func newBoltHeartbeatAck(protocol byte, requestId uint32) *sofarpc.BoltResponseCommand{
+	return &sofarpc.BoltResponseCommand{
+		Protocol: protocol,
+		CmdType:  sofarpc.RESPONSE,
+		CmdCode:  sofarpc.HEARTBEAT,
+		Version: 1,
+		ReqId: requestId,
+		CodecPro: codec.HESSIAN2_SERIALIZE,//todo: read default codec from config
+		ResponseStatus: sofarpc.RESPONSE_STATUS_SUCCESS,
+	}
 }
 
 func (f *healthCheckFilter) SetDecoderFilterCallbacks(cb types.StreamDecoderFilterCallbacks) {
