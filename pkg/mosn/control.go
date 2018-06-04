9a2bc0844f20cefd7245abba202364305436c33b
烈元 <lieyuan@taobao.com>
2018-06-04 09:46:06 +0800

    fix file and directory

--- pkg/mosn/control.go
+++ pkg/mosn/control.go
@@ -14,7 +14,7 @@ var (
 				Name:   "config, c",
 				Usage:  "Load configuration from `FILE`",
 				EnvVar: "MOSN_CONFIG",
-				Value:  "resource/mson_config.json",
+				Value:  "resource/mosn_config.json",
 			},
 		},
 		Action: func(c *cli.Context) error {
