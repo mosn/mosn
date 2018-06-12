5f9885ec92a1404f6f455f59ca13053d92d2182a
lingtao.klt <lingtao.klt@alibaba-inc.com>
2018-06-12 23:33:32 +0800

    add RR loadbalancer

--- pkg/upstream/cluster/loadbalancer.go
+++ pkg/upstream/cluster/loadbalancer.go
@@ -1,16 +1,22 @@
 package cluster
 
 import (
-	"context"
-	"gitlab.alipay-inc.com/afe/mosn/pkg/log"
-	"gitlab.alipay-inc.com/afe/mosn/pkg/types"
-	"math/rand"
+"context"
+"math/rand"
+
+
+"gitlab.alipay-inc.com/afe/mosn/pkg/log"
+"gitlab.alipay-inc.com/afe/mosn/pkg/types"
+
+
 )
 
 func NewLoadBalancer(lbType types.LoadBalancerType, prioritySet types.PrioritySet) types.LoadBalancer {
 	switch lbType {
 	case types.Random:
 		return newRandomLoadbalancer(prioritySet)
+	case types.RoundRobin:
+		return newRoundRobinLoadBalancer(prioritySet)
 	}
 
 	return nil
@@ -21,19 +27,19 @@ type loadbalaner struct {
 }
 
 // Random LoadBalancer
-type randomloadbalancer struct {
+type randomLoadBalancer struct {
 	loadbalaner
 }
 
 func newRandomLoadbalancer(prioritySet types.PrioritySet) types.LoadBalancer {
-	return &randomloadbalancer{
+	return &randomLoadBalancer{
 		loadbalaner: loadbalaner{
 			prioritySet: prioritySet,
 		},
 	}
 }
 
-func (l *randomloadbalancer) ChooseHost(context context.Context) types.Host {
+func (l *randomLoadBalancer) ChooseHost(context context.Context) types.Host {
 	hostSets := l.prioritySet.HostSetsByPriority()
 	idx := rand.Intn(len(hostSets))
 	hostset := hostSets[idx]
@@ -50,3 +56,45 @@ func (l *randomloadbalancer) ChooseHost(context context.Context) types.Host {
 }
 
 // TODO: more loadbalancers@boqin
+type roundRobinLoadBalancer struct {
+	loadbalaner
+	// rrIndex for hostSet select
+	rrIndexPriority uint32
+	// rrInde for host select
+	rrIndex         uint32
+}
+
+func newRoundRobinLoadBalancer(prioritySet types.PrioritySet) types.LoadBalancer {
+	return &roundRobinLoadBalancer{
+		loadbalaner: loadbalaner{
+			prioritySet: prioritySet,
+		},
+	}
+}
+
+func (l *roundRobinLoadBalancer) ChooseHost(context context.Context) types.Host {
+	var selectedHostSet []types.Host
+	
+	hostSets := l.prioritySet.HostSetsByPriority()
+	hostSetsNum := uint32(len(hostSets))
+	curHostSet := hostSets[l.rrIndexPriority % hostSetsNum].HealthyHosts()
+	
+	if l.rrIndex >= uint32(len(curHostSet)) {
+		l.rrIndexPriority = (l.rrIndexPriority + 1) % hostSetsNum
+		l.rrIndex = 0
+		selectedHostSet = hostSets[l.rrIndexPriority].HealthyHosts()
+	} else {
+		selectedHostSet = curHostSet
+	}
+	
+	if len(selectedHostSet) == 0 {
+		logger := log.ByContext(context)
+		logger.Debugf("Choose host in RoundRobin failed, no health host found")
+		return nil
+	}
+	
+	selectedHost := selectedHostSet[l.rrIndex % uint32(len(selectedHostSet))]
+	l.rrIndex ++
+	
+	return selectedHost
+}
---
+++ pkg/upstream/cluster/loadbalancer_test.go
@@ -0,0 +1,58 @@
+package cluster
+
+import (
+	"testing"
+
+	"gitlab.alipay-inc.com/afe/mosn/pkg/api/v2"
+	"gitlab.alipay-inc.com/afe/mosn/pkg/types"
+)
+
+func Test_roundRobinLoadBalancer_ChooseHost(t *testing.T) {
+
+	host1 := NewHost(v2.Host{Address: "127.0.0.1", Hostname: "test", Weight: 0}, nil)
+	host2 := NewHost(v2.Host{Address: "127.0.0.2", Hostname: "test2", Weight: 0}, nil)
+	host3 := NewHost(v2.Host{Address: "127.0.0.3", Hostname: "test", Weight: 0}, nil)
+	host4 := NewHost(v2.Host{Address: "127.0.0.4", Hostname: "test2", Weight: 0}, nil)
+	host5 := NewHost(v2.Host{Address: "127.0.0.5", Hostname: "test2", Weight: 0}, nil)
+
+	hosts1 := []types.Host{host1, host2}
+	hosts2 := []types.Host{host3, host4}
+	hosts3:= []types.Host{host5}
+	
+
+	hs1 := hostSet{
+		hosts: hosts1,
+	}
+
+	hs2 := hostSet{
+		hosts: hosts2,
+	}
+	
+	hs3 := hostSet{
+		hosts:hosts3,
+	}
+
+	hostset := []types.HostSet{&hs1, &hs2,&hs3}
+
+	prioritySet := prioritySet{
+		hostSets: hostset,
+	}
+
+	loadbalaner := loadbalaner{
+		prioritySet: &prioritySet,
+	}
+
+	l := &roundRobinLoadBalancer{
+		loadbalaner: loadbalaner,
+	}
+
+	want := []types.Host{host1, host2, host3, host4,host5}
+
+	for i := 0; i < len(want); i++ {
+		got := l.ChooseHost(nil)
+		if got != want[i] {
+			t.Fatal("Test Error in case %d , got %+v, but want %+v,", i, got, want)
+		}
+
+	}
+}
