4d74bf6684b9c900238238efa492633e70083731
wugou.cyf <wugou.cyf@antfin.com>
2018-06-29 14:26:35 +0800

    add vendor

---
+++ vendor/github.com/markphelps/optional/generate.go
@@ -0,0 +1,22 @@
+//go:generate go run cmd/optional/main.go -type=bool -output=Bool
+//go:generate go run cmd/optional/main.go -type=byte -output=Byte
+//go:generate go run cmd/optional/main.go -type=complex128 -output=Complex128
+//go:generate go run cmd/optional/main.go -type=complex64 -output=Complex64
+//go:generate go run cmd/optional/main.go -type=error -output=Error
+//go:generate go run cmd/optional/main.go -type=float32 -output=Float32
+//go:generate go run cmd/optional/main.go -type=float64 -output=Float64
+//go:generate go run cmd/optional/main.go -type=int -output=Int
+//go:generate go run cmd/optional/main.go -type=int16 -output=Int16
+//go:generate go run cmd/optional/main.go -type=int32 -output=Int32
+//go:generate go run cmd/optional/main.go -type=int64 -output=Int64
+//go:generate go run cmd/optional/main.go -type=int8 -output=Int8
+//go:generate go run cmd/optional/main.go -type=rune -output=Rune
+//go:generate go run cmd/optional/main.go -type=string -output=String
+//go:generate go run cmd/optional/main.go -type=uint -output=Uint
+//go:generate go run cmd/optional/main.go -type=uint16 -output=Uint16
+//go:generate go run cmd/optional/main.go -type=uint32 -output=Uint32
+//go:generate go run cmd/optional/main.go -type=uint64 -output=Uint64
+//go:generate go run cmd/optional/main.go -type=uint8 -output=Uint8
+//go:generate go run cmd/optional/main.go -type=uintptr -output=Uintptr
+
+package optional
