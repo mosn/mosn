8b831777f8a6d57450342ee14122588e3515d15c
Lingtaonju <itkltao@sina.com>
2018-07-18 22:17:24 +0800

    add missing file for last commit

---
+++ cmd/mosn/main/control.go
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"github.com/alipay/sofa-mosn/pkg/config"
+	"github.com/alipay/sofa-mosn/cmd/mosn"
+	"github.com/urfave/cli"
+)
+
+var (
+	cmdStart = cli.Command{
+		Name:  "start",
+		Usage: "start mosn proxy",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:   "config, c",
+				Usage:  "Load configuration from `FILE`",
+				EnvVar: "MOSN_CONFIG",
+				Value:  "configs/mosn_config.json",
+			}, cli.StringFlag{
+				Name:   "service-cluster, s",
+				Usage:  "sidecar service cluster",
+				EnvVar: "SERVICE_CLUSTER",
+			}, cli.StringFlag{
+				Name:   "service-node, n",
+				Usage:  "sidecar service node",
+				EnvVar: "SERVICE_NODE",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			configPath := c.String("config")
+			serviceCluster := c.String("service-cluster")
+			serviceNode := c.String("service-node")
+			conf := config.Load(configPath)
+			mosn.Start(conf, serviceCluster, serviceNode)
+			return nil
+		},
+	}
+
+	cmdStop = cli.Command{
+		Name:  "stop",
+		Usage: "stop mosn proxy",
+		Action: func(c *cli.Context) error {
+			return nil
+		},
+	}
+
+	cmdReload = cli.Command{
+		Name:  "reload",
+		Usage: "reconfiguration",
+		Action: func(c *cli.Context) error {
+			return nil
+		},
+	}
+)
