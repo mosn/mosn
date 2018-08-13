74d9ec1361785c2bd1048a4a3ee4f4774e306915
neverhook <neverhook430@gmail.com>
2018-08-13 13:24:12 +0800

    add benchmark with pure-unbound channels

--- pkg/sync/workerpool_test.go
+++ pkg/sync/workerpool_test.go
@@ -22,7 +22,6 @@ func TestJobOrder(t *testing.T) {
 	shardEvents := 512
 	wg := sync.WaitGroup{}
 
-
 	consumer := func(shard int, jobChan <-chan interface{}) {
 		prev := 0
 		count := 0
@@ -52,12 +51,11 @@ func TestJobOrder(t *testing.T) {
 	pool, _ := NewShardWorkerPool(shardsNum*64, shardsNum, consumer)
 	pool.Init()
 
-
 	// multi goroutine offer is not guaranteed FIFO order, because race condition may happen in Offer method
 	// so we let the producer and consumer to be one-to-one relation.
 	for i := 0; i < shardsNum; i ++ {
 		wg.Add(1)
-		counter := uint32(i )
+		counter := uint32(i)
 		go func() {
 			for j := 0; j < shardEvents; j++ {
 				pool.Offer(&TestJob{i: atomic.AddUint32(&counter, uint32(shardsNum))})
@@ -66,4 +64,156 @@ func TestJobOrder(t *testing.T) {
 	}
 
 	wg.Wait()
+}
+
+func eventProcess(b *testing.B) {
+	shardEvents := 512
+	wg := sync.WaitGroup{}
+
+	consumer := func(shard int, jobChan <-chan interface{}) {
+		prev := 0
+		count := 0
+
+		for job := range jobChan {
+			if testJob, ok := job.(*TestJob); ok {
+				if int(testJob.i) <= prev {
+					b.Errorf("unexpected event order, shard %d, prev %d, curr %d", shard, prev, testJob.i)
+					wg.Done()
+					return
+				}
+
+				prev = int(testJob.i)
+				count++
+
+				if count >= shardEvents {
+					wg.Done()
+					return
+				}
+			}
+		}
+
+	}
+
+	shardsNum := runtime.NumCPU()
+	// shard cap is 64
+	pool, _ := NewShardWorkerPool(shardsNum*64, shardsNum, consumer)
+	pool.Init()
+
+	// multi goroutine offer is not guaranteed FIFO order, because race condition may happen in Offer method
+	// so we let the producer and consumer to be one-to-one relation.
+	for i := 0; i < shardsNum; i ++ {
+		wg.Add(1)
+		counter := uint32(i)
+		go func() {
+			for j := 0; j < shardEvents; j++ {
+				pool.Offer(&TestJob{i: atomic.AddUint32(&counter, uint32(shardsNum))})
+			}
+		}()
+	}
+
+	wg.Wait()
+}
+
+func BenchmarkShardWorkerPool(b *testing.B) {
+	log.InitDefaultLogger("stdout", log.ERROR)
+
+	for i := 0; i < b.N; i ++ {
+		eventProcess(b)
+	}
+}
+
+// Implements from https://medium.com/capital-one-developers/building-an-unbounded-channel-in-go-789e175cd2cd
+func MakeInfinite() (chan<- interface{}, <-chan interface{}) {
+	in := make(chan interface{})
+	out := make(chan interface{})
+
+	go func() {
+		var inQueue []interface{}
+		outCh := func() chan interface{} {
+			if len(inQueue) == 0 {
+				return nil
+			}
+			return out
+		}
+
+		curVal := func() interface{} {
+			if len(inQueue) == 0 {
+				return nil
+			}
+			return inQueue[0]
+		}
+
+		for len(inQueue) > 0 || in != nil {
+			select {
+			case v, ok := <-in:
+				if !ok {
+					in = nil
+				} else {
+					inQueue = append(inQueue, v)
+				}
+			case outCh() <- curVal():
+				inQueue = inQueue[1:]
+			}
+		}
+		close(out)
+	}()
+	return in, out
+}
+
+func eventProcessWithUnboundedChannel(b *testing.B){
+	shardEvents := 512
+	wg := sync.WaitGroup{}
+
+	consumer := func(shard int, jobChan <-chan interface{}) {
+		prev := 0
+		count := 0
+
+		for job := range jobChan {
+			if testJob, ok := job.(*TestJob); ok {
+				if int(testJob.i) <= prev {
+					b.Errorf("unexpected event order, shard %d, prev %d, curr %d", shard, prev, testJob.i)
+					wg.Done()
+					return
+				}
+
+				prev = int(testJob.i)
+				count++
+
+				if count >= shardEvents {
+					wg.Done()
+					return
+				}
+			}
+		}
+
+	}
+
+	shardsNum := runtime.NumCPU()
+
+	// multi goroutine offer is not guaranteed FIFO order, because race condition may happen in Offer method
+	// so we let the producer and consumer to be one-to-one relation.
+	for i := 0; i < shardsNum; i ++ {
+		wg.Add(1)
+		counter := uint32(i)
+		in, out := MakeInfinite()
+
+		go func() {
+			for j := 0; j < shardEvents; j++ {
+				in <- &TestJob{i: atomic.AddUint32(&counter, uint32(shardsNum))}
+			}
+		}()
+		go func() {
+			consumer(i, out)
+		}()
+	}
+
+	wg.Wait()
+}
+
+func BenchmarkUnboundChannel(b *testing.B) {
+	log.InitDefaultLogger("stdout", log.ERROR)
+
+	for i := 0; i < b.N; i ++ {
+		eventProcessWithUnboundedChannel(b)
+	}
 }
