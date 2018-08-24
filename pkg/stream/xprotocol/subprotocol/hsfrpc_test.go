e282e41a310ed1b76ddb382853a43e1b886b44b5
lazyrabbit2004 <lazyrabbit2004@163.com>
2018-08-24 16:41:55 +0800

    1. interface SplitRequest ==> SplitFrame

--- pkg/stream/xprotocol/subprotocol/hsfrpc_test.go
+++ pkg/stream/xprotocol/subprotocol/hsfrpc_test.go
@@ -21,22 +21,22 @@ import (
 	"testing"
 )
 
-func Test_SplitRequest_01(t *testing.T) {
-	msg := []byte{0x0e, 1, 1, 0, 2, 0, 0, 0, 0x00, 0x00, 0x00, 0x00, 0x12, 0x34, 0x56, 0x78, 30}
+func Test_SplitFrame_01(t *testing.T) {
+	msg := []byte{0x0e, 1, 1, 0, 2, 0, 0, 0, 0x00, 0x00, 0x00, 0x00, 0x12, 0x34, 0x56, 0x78, 0, 0, 0, 0}
 	rpc := NewRPCHSF()
-	reqs := rpc.SplitRequest(msg)
+	reqs := rpc.SplitFrame(msg)
 	reqsLen := len(reqs)
-	if reqsLen != 0 {
-		t.Errorf("%d != 0", reqsLen)
+	if reqsLen != 1 {
+		t.Errorf("%d != 1", reqsLen)
 	} else {
-		t.Log("split response ok")
+		t.Log("split response succ ok")
 	}
 }
 
-func Test_SplitRequest_02(t *testing.T) {
+func Test_SplitFrame_02(t *testing.T) {
 	msg := []byte{0x0e, 1, 2, 0, 2, 0, 0, 0, 0x00, 0x00, 0x00, 0x00, 0x12, 0x34, 0x56, 0x78, 30}
 	rpc := NewRPCHSF()
-	reqs := rpc.SplitRequest(msg)
+	reqs := rpc.SplitFrame(msg)
 	reqsLen := len(reqs)
 	if reqsLen != 0 {
 		t.Errorf("%d != 0", reqsLen)
@@ -45,10 +45,10 @@ func Test_SplitRequest_02(t *testing.T) {
 	}
 }
 
-func Test_SplitRequest_03(t *testing.T) {
+func Test_SplitFrame_03(t *testing.T) {
 	msg := []byte{0x0e, 1, 0, 2, 0, 0, 0, 0x00, 0x00, 0x00, 0x00, 0x12, 0x34, 0x56, 0x78, 0, 0, 3, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'a', 'b', 0x0e, 1, 0, 2, 0, 0, 0, 0x00, 0x00, 0x00, 0x00, 0x12, 0x34, 0x56, 0x79, 0, 0, 3, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'c', 'd', 0x0e, 1, 0, 2, 0, 0, 0, 0x00, 0x00, 0x00, 0x00, 0x12, 0x34, 0x56, 0x77, 0, 0, 3, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'e', 'f'}
 	rpc := NewRPCHSF()
-	reqs := rpc.SplitRequest(msg)
+	reqs := rpc.SplitFrame(msg)
 	reqsLen := len(reqs)
 	if reqsLen != 3 {
 		t.Errorf("%d != 3", reqsLen)
@@ -57,10 +57,10 @@ func Test_SplitRequest_03(t *testing.T) {
 	}
 }
 
-func Test_SplitRequest_04(t *testing.T) {
+func Test_SplitFrame_04(t *testing.T) {
 	msg := []byte{0x0e, 1, 0, 2, 0, 0, 0, 0x00, 0x00, 0x00, 0x00, 0x12, 0x34, 0x56, 0x78, 0, 0, 3, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'a', 'b', 0x0e, 1, 0, 2, 0, 0, 0, 0x00, 0x00, 0x00, 0x00, 0x12, 0x34, 0x56, 0x79, 0, 0, 3, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'c'}
 	rpc := NewRPCHSF()
-	reqs := rpc.SplitRequest(msg)
+	reqs := rpc.SplitFrame(msg)
 	reqsLen := len(reqs)
 	if reqsLen != 1 {
 		t.Errorf("%d != 1", reqsLen)
@@ -69,10 +69,10 @@ func Test_SplitRequest_04(t *testing.T) {
 	}
 }
 
-func Test_SplitRequest_05(t *testing.T) {
+func Test_SplitFrame_05(t *testing.T) {
 	msg := []byte{14, 1, 0, 4, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 208, 0, 0, 0, 45, 0, 0, 0, 8, 0, 0, 0, 1, 0, 0, 0, 16, 0, 0, 0, 6, 0, 0, 0, 143, 99, 111, 109, 46, 116, 101, 115, 116, 46, 112, 97, 110, 100, 111, 114, 97, 46, 104, 115, 102, 46, 72, 101, 108, 108, 111, 83, 101, 114, 118, 105, 99, 101, 58, 49, 46, 48, 46, 48, 46, 100, 97, 105, 108, 121, 115, 97, 121, 72, 101, 108, 108, 111, 106, 97, 118, 97, 46, 108, 97, 110, 103, 46, 83, 116, 114, 105, 110, 103, 5, 119, 111, 114, 108, 100, 72, 16, 67, 111, 110, 115, 117, 109, 101, 114, 45, 65, 112, 112, 78, 97, 109, 101, 15, 104, 115, 102, 45, 115, 101, 114, 118, 101, 114, 45, 100, 101, 109, 111, 12, 116, 97, 114, 103, 101, 116, 95, 103, 114, 111, 117, 112, 4, 72, 83, 70, 49, 4, 95, 84, 73, 68, 78, 16, 101, 97, 103, 108, 101, 101, 121, 101, 95, 99, 111, 110, 116, 101, 120, 116, 72, 7, 116, 114, 97, 99, 101, 73, 100, 30, 48, 97, 57, 55, 54, 51, 54, 55, 49, 53, 51, 53, 48, 56, 50, 55, 56, 54, 49, 48, 55, 49, 48, 48, 49, 100, 48, 48, 54, 49, 5, 114, 112, 99, 73, 100, 1, 57, 16, 101, 97, 103, 108, 101, 69, 121, 101, 85, 115, 101, 114, 68, 97, 116, 97, 78, 90, 90}
 	rpc := NewRPCHSF()
-	reqs := rpc.SplitRequest(msg)
+	reqs := rpc.SplitFrame(msg)
 	reqsLen := len(reqs)
 	if reqsLen != 1 {
 		t.Errorf("%d != 1", reqsLen)
@@ -81,15 +81,15 @@ func Test_SplitRequest_05(t *testing.T) {
 	}
 }
 
-func Test_SplitRequest_06(t *testing.T) {
+func Test_SplitFrame_06(t *testing.T) {
 	msg := []byte{14, 1, 0, 4, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 208, 0, 0, 0, 45, 0, 0, 0, 8, 0, 0, 0, 1, 0, 0, 0, 16, 0, 0, 0, 6, 0, 0, 0, 143, 99, 111, 109, 46, 116, 101, 115, 116, 46, 112, 97, 110, 100, 111, 114, 97, 46, 104, 115, 102, 46, 72, 101, 108, 108, 111, 83, 101, 114, 118, 105, 99, 101, 58, 49, 46, 48, 46, 48, 46, 100, 97, 105, 108, 121, 115, 97, 121, 72, 101, 108, 108, 111, 106, 97, 118, 97, 46, 108, 97, 110, 103, 46, 83, 116, 114, 105, 110, 103, 5, 119, 111, 114, 108, 100, 72, 16, 67, 111, 110, 115, 117, 109, 101, 114, 45, 65, 112, 112, 78, 97, 109, 101, 15, 104, 115, 102, 45, 115, 101, 114, 118, 101, 114, 45, 100, 101, 109, 111, 12, 116, 97, 114, 103, 101, 116, 95, 103, 114, 111, 117, 112, 4, 72, 83, 70, 49, 4, 95, 84, 73, 68, 78, 16, 101, 97, 103, 108, 101, 101, 121, 101, 95, 99, 111, 110, 116, 101, 120, 116, 72, 7, 116, 114, 97, 99, 101, 73, 100, 30, 48, 97, 57, 55, 54, 51, 54, 55, 49, 53, 51, 53, 48, 56, 50, 55, 56, 54, 49, 48, 55, 49, 48, 48, 49, 100, 48, 48, 54, 49, 5, 114, 112, 99, 73, 100, 1, 57, 16, 101, 97, 103, 108, 101, 69, 121, 101, 85, 115, 101, 114, 68, 97, 116, 97, 78, 90, 90, 12, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 11, 184, 12, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 11, 184, 12, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 11, 184}
 	rpc := NewRPCHSF()
-	reqs := rpc.SplitRequest(msg)
+	reqs := rpc.SplitFrame(msg)
 	reqsLen := len(reqs)
-	if reqsLen != 1 {
-		t.Errorf("%d != 1", reqsLen)
+	if reqsLen != 4 {
+		t.Errorf("%d != 4", reqsLen)
 	} else {
-		t.Log("split request follow with heart-beat msg ok")
+		t.Log("split request follow with heart-beat frame ok")
 	}
 }
 
