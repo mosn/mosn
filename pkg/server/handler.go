15a63b182c6deb4c8930066704bfd4c951c6ae2a
nejisama <neji_bupt@163.com>
2018-08-24 12:21:26 +0800

    use uuid for default listener id

--- pkg/server/handler.go
+++ pkg/server/handler.go
@@ -20,6 +20,7 @@ package server
 import (
 	"container/list"
 	"context"
+	"crypto/rand"
 	"fmt"
 	"net"
 	"os"
@@ -87,23 +88,33 @@ func (ch *connHandler) NumConnections() uint64 {
 	return uint64(atomic.LoadInt64(&ch.numConnections))
 }
 
-var listenerName uint32
-
-func GenerateListenerID() string {
-	atomic.AddUint32(&listenerName, 1)
-	return strconv.FormatUint(uint64(listenerName), 10)
+// GenerateListenerID generates an uuid
+// https://tools.ietf.org/html/rfc4122
+// crypto.rand use getrandom(2) or /dev/urandom
+// It is maybe occur an error due to system error
+// panic if an error occurred
+func (ch *connHandler) GenerateListenerID() string {
+	uuid := make([]byte, 16)
+	_, err := rand.Read(uuid)
+	if err != nil {
+		ch.logger.Fatalf("generate an uuid failed, error: %v", err)
+	}
+	// see section 4.1.1
+	uuid[8] = uuid[8]&^0xc0 | 0x80
+	// see section 4.1.3
+	uuid[6] = uuid[6]&^0xf0 | 0x40
+	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:])
 }
 
 // AddOrUpdateListener used to add or update listener
 // listener name is unique key to represent the listener
 // and listener with the same name must have the same configured address
-
 func (ch *connHandler) AddOrUpdateListener(lc *v2.ListenerConfig, networkFiltersFactories []types.NetworkFilterChainFactory,
 	streamFiltersFactories []types.StreamFilterChainFactory) types.ListenerEventListener {
 
 	var listenerName string
 	if lc.Name == "" {
-		listenerName = GenerateListenerID()
+		listenerName = ch.GenerateListenerID()
 		lc.Name = listenerName
 	} else {
 		listenerName = lc.Name
