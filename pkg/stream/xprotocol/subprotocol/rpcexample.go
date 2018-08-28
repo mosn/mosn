08115d8b138c9b50409b5987831a11f87e35581d
zewen.pzw <zewen.pzw@alibaba-inc.com>
2018-08-28 22:55:58 +0800

    add rpc-example unit test

--- pkg/stream/xprotocol/subprotocol/rpcexample.go
+++ pkg/stream/xprotocol/subprotocol/rpcexample.go
@@ -3,6 +3,13 @@ package subprotocol
 import (
 	"context"
 
+	"encoding/binary"
+	"fmt"
+
+	"bytes"
+	"strconv"
+
+	"github.com/alipay/sofa-mosn/pkg/log"
 	"github.com/alipay/sofa-mosn/pkg/types"
 )
 
@@ -13,23 +20,68 @@ func init() {
 type rpcExampleFactory struct{}
 
 func (ref *rpcExampleFactory) CreateSubProtocolCodec(context context.Context) types.Multiplexing {
-	return newRPCExample()
+	return NewRPCExample()
 }
 
 type rpcExample struct{}
 
-func newRPCExample() types.Multiplexing {
+// NewRPCExample create rpc-example codec
+func NewRPCExample() types.Multiplexing {
 	return &rpcExample{}
 }
 
+var (
+	// ReqIDLen rpc-example req id len
+	ReqIDLen = 8
+	// ReqIDBeginOffset rpc-example req id field offset
+	ReqIDBeginOffset = 8
+	// ReqDataLen rpc-example request package size
+	ReqDataLen = 16
+)
+
 func (re *rpcExample) SplitFrame(data []byte) [][]byte {
-	return nil
+	var reqs [][]byte
+	start := 0
+	dataLen := len(data)
+	for true {
+		if dataLen >= ReqDataLen {
+			// there is one valid rpc-example request
+			reqs = append(reqs, data[start:(start+ReqDataLen)])
+			start += ReqDataLen
+			dataLen -= ReqDataLen
+			if dataLen == 0 {
+				break
+			}
+		} else {
+			// invalid data
+			log.DefaultLogger.Tracef("[SplitFrame] over! remain data len = %d", dataLen)
+			break
+		}
+	}
+	return reqs
 }
 
 func (re *rpcExample) GetStreamID(data []byte) string {
-	return ""
+	reqIDRaw := data[8:]
+	reqID := binary.BigEndian.Uint64(reqIDRaw)
+	reqIDStr := fmt.Sprintf("%d", reqID)
+	return reqIDStr
 }
 
 func (re *rpcExample) SetStreamID(data []byte, streamID string) []byte {
-	return nil
+	reqID, err := strconv.ParseInt(streamID, 10, 64)
+	if err != nil {
+		return data
+	}
+	buf := bytes.Buffer{}
+	err = binary.Write(&buf, binary.BigEndian, reqID)
+	if err != nil {
+		return data
+	}
+	reqIDStr := buf.Bytes()
+	reqIDStrLen := len(reqIDStr)
+	for i := 0; i < ReqIDLen && i <= reqIDStrLen; i++ {
+		data[ReqIDBeginOffset+i] = reqIDStr[i]
+	}
+	return data
 }
