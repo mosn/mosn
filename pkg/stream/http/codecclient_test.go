0475605527b7d79ee35266054f4842b373becd02
lieyuan <lieyuan@taobao.com>
2018-09-13 17:45:22 +0800

    minor change

--- pkg/stream/http/codecclient_test.go
+++ pkg/stream/http/codecclient_test.go
@@ -66,7 +66,7 @@ func checkNumbers(t *testing.T, codecClient str.CodecClient, want int) {
 }
 
 func TestActiveRequests(t *testing.T) {
-	cli := NewMockClient(t)
+	cli := newMockClient(t)
 	host := cluster.NewHost(v2.Host{
 		HostConfig: v2.HostConfig{
 			Address: "127.0.0.1", Hostname: "test", Weight: 0,
