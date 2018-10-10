75f64e84bc634215e173405b39dd72c499a98d7d
khotyn <hting1@gmail.com>
2018-10-10 13:10:39 +0800

    Improve code coverage.

--- pkg/trace/sofatracer_test.go
+++ pkg/trace/sofatracer_test.go
@@ -1,6 +1,15 @@
 package trace
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
+
+func TestSofaTracerStartFinish(t *testing.T) {
+	span := SofaTracerInstance.Start(time.Now())
+	span.SetTag(TRACE_ID, IdGen().GenerateTraceId())
+	span.FinishSpan()
+}
 
 func TestSofaTracerPrintSpan(t *testing.T) {
 	SofaTracerInstance.printSpan(&SofaTracerSpan{})
@@ -10,6 +19,7 @@ func TestSofaTracerPrintIngressSpan(t *testing.T) {
 	span := &SofaTracerSpan{
 		tags: map[string]string{},
 	}
+	span.tags[DOWNSTEAM_HOST_ADDRESS] = "127.0.0.1:43"
 	span.tags[SPAN_TYPE] = "ingress"
 	SofaTracerInstance.printSpan(span)
 }
