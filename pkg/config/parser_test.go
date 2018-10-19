caca0d2d9c357d243aa7317fc8eaa53dbfd93b7d
zewen.pzw <zewen.pzw@alibaba-inc.com>
2018-10-19 12:06:34 +0800

    fix parser test bug

--- pkg/config/parser_test.go
+++ pkg/config/parser_test.go
@@ -300,11 +300,26 @@ func TestParseHealthCheckFilter(t *testing.T) {
 
 func TestParseTCPProxy(t *testing.T) {
 	m := map[string]interface{}{
+		"stat_prefix":          "tcp_proxy",
+		"cluster":              "cluster",
+		"max_connect_attempts": 1000,
 		"routes": []interface{}{
 			map[string]interface{}{
-				"cluster":           "test",
-				"source_addrs":      []string{"127.0.0.1:80"},
-				"destination_addrs": []string{"127.0.0.1:8080"},
+				"cluster": "test",
+				"SourceAddrs": []interface{}{
+					map[string]interface{}{
+						"address": "127.0.0.1",
+						"length":  32,
+					},
+				},
+				"DestinationAddrs": []interface{}{
+					map[string]interface{}{
+						"address": "127.0.0.1",
+						"length":  32,
+					},
+				},
+				"SourcePort":      "8080",
+				"DestinationPort": "8080",
 			},
 		},
 	}
@@ -319,10 +334,14 @@ func TestParseTCPProxy(t *testing.T) {
 		r := tcpproxy.Routes[0]
 		if !(r.Cluster == "test" &&
 			len(r.SourceAddrs) == 1 &&
-			r.SourceAddrs[0].String() == "127.0.0.1:80" &&
+			r.SourceAddrs[0].Address == "127.0.0.1" &&
+			r.SourceAddrs[0].Length == 32 &&
 			len(r.DestinationAddrs) == 1 &&
-			r.DestinationAddrs[0].String() == "127.0.0.1:8080") {
-			t.Error("parse tcpproxy failed")
+			r.DestinationAddrs[0].Address == "127.0.0.1" &&
+			r.DestinationAddrs[0].Length == 32 &&
+			r.SourcePort == "8080" &&
+			r.DestinationPort == "8080") {
+			t.Error("route failed")
 		}
 	}
 }
