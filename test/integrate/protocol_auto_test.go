4621b1dcd6edbe9441703afa3305aff038f04686
lieyuan <lieyuan@taobao.com>
2018-11-29 18:19:17 +0800

    add protocol auto test

--- test/integrate/protocol_auto_test.go
+++ test/integrate/protocol_auto_test.go
@@ -4,15 +4,18 @@ import (
 	"testing"
 	"time"
 
+	"github.com/alipay/sofa-mosn/pkg/module/http2"
 	"github.com/alipay/sofa-mosn/pkg/mosn"
 	"github.com/alipay/sofa-mosn/pkg/protocol"
 	_ "github.com/alipay/sofa-mosn/pkg/protocol/rpc/sofarpc/codec"
 	_ "github.com/alipay/sofa-mosn/pkg/protocol/rpc/sofarpc/conv"
+	"github.com/alipay/sofa-mosn/pkg/stream"
 	_ "github.com/alipay/sofa-mosn/pkg/stream/http"
 	_ "github.com/alipay/sofa-mosn/pkg/stream/http2"
 	_ "github.com/alipay/sofa-mosn/pkg/stream/mhttp2"
 	_ "github.com/alipay/sofa-mosn/pkg/stream/sofarpc"
 	_ "github.com/alipay/sofa-mosn/pkg/stream/xprotocol"
+	"github.com/alipay/sofa-mosn/pkg/types"
 	"github.com/alipay/sofa-mosn/test/util"
 )
 
@@ -38,7 +41,6 @@ func TestAuto(t *testing.T) {
 	testCases := []*TestCase{
 		NewTestCase(t, protocol.MHTTP2, protocol.MHTTP2, util.NewUpstreamHTTP2(t, appaddr, nil)),
 		NewTestCase(t, protocol.HTTP1, protocol.HTTP1, util.NewHTTPServer(t, nil)),
-
 	}
 	for i, tc := range testCases {
 		t.Logf("start case #%d\n", i)
@@ -62,7 +64,6 @@ func TestAutoTLS(t *testing.T) {
 	testCases := []*TestCase{
 		NewTestCase(t, protocol.MHTTP2, protocol.MHTTP2, util.NewUpstreamHTTP2(t, appaddr, nil)),
 		NewTestCase(t, protocol.HTTP1, protocol.HTTP1, util.NewHTTPServer(t, nil)),
-
 	}
 	for i, tc := range testCases {
 		t.Logf("start case #%d\n", i)
@@ -78,5 +79,58 @@ func TestAutoTLS(t *testing.T) {
 		}
 		close(tc.Stop)
 		time.Sleep(time.Second)
+	}
+}
+
+func TestProtocolHttp2(t *testing.T) {
+	var prot types.Protocol
+	var magic string
+	var err error
+
+	magic = http2.ClientPreface
+	prot, err = stream.SelectStreamFactoryProtocol("", []byte(magic))
+	if prot != protocol.MHTTP2 {
+		t.Errorf("[ERROR MESSAGE] type error magic : %v\n", magic)
+	}
+
+	len := len(http2.ClientPreface)
+	prot, err = stream.SelectStreamFactoryProtocol("", []byte(magic)[0:len-1])
+	if err != stream.EAGAIN {
+		t.Errorf("[ERROR MESSAGE] type error protocol :%v", err)
+	}
+
+	prot, err = stream.SelectStreamFactoryProtocol("", []byte("helloworld"))
+	if err != stream.FAILED {
+		t.Errorf("[ERROR MESSAGE] type error protocol :%v", err)
+	}
+}
+
+func TestProtocolHttp1(t *testing.T) {
+	var prot types.Protocol
+	var magic string
+	var err error
+
+	magic = "GET"
+	prot, err = stream.SelectStreamFactoryProtocol("", []byte(magic))
+	if prot != protocol.HTTP1 {
+		t.Errorf("[ERROR MESSAGE] type error magic : %v\n", magic)
+	}
+
+	magic = "POST"
+	prot, err = stream.SelectStreamFactoryProtocol("", []byte(magic))
+	if prot != protocol.HTTP1 {
+		t.Errorf("[ERROR MESSAGE] type error magic : %v\n", magic)
+	}
+
+	magic = "POS"
+	prot, err = stream.SelectStreamFactoryProtocol("", []byte(magic))
+	if err != stream.EAGAIN {
+		t.Errorf("[ERROR MESSAGE] type error protocol :%v", err)
+	}
+
+	magic = "PPPPPPP"
+	prot, err = stream.SelectStreamFactoryProtocol("", []byte(magic))
+	if err != stream.FAILED {
+		t.Errorf("[ERROR MESSAGE] type error protocol :%v", err)
 	}
 }
