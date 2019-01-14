c27ff6cd2274609b863d02fdf98cec750fd83b75
nejisama <neji_bupt@163.com>
2019-01-14 18:11:30 +0800

    fix ci, go test may exit 1 directly

--- etc/script/report.sh
+++ etc/script/report.sh
@@ -5,14 +5,8 @@ echo "" > coverage.txt
 
 for d in $(go list ./pkg/...); do
     echo "--------Run test package: $d"
-    output="$(go test -v -coverprofile=profile.out -covermode=atomic $d)"
-    echo "$output"
-    fail=$(echo "$output" | grep "\-\-\- FAIL:" | wc -l)
+    go test -v -coverprofile=profile.out -covermode=atomic $d
     echo "--------Finish test package: $d"
-    if [ $fail -ne 0 ]; then
-	    echo "-------Package $d test FAILED"
-	    exit 1
-    fi
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
