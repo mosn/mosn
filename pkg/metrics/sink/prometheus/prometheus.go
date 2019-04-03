2492a3aa5023aac018bbdee8aaa6d0da37133efa
neverhook <neverhook430@gmail.com>
2019-04-03 14:46:54 +0800

    native support for plain text prometheus output

--- pkg/metrics/sink/prometheus/prometheus.go
+++ pkg/metrics/sink/prometheus/prometheus.go
@@ -32,12 +32,14 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/alipay/sofa-mosn/pkg/metrics"
 	"io"
-	"github.com/prometheus/common/expfmt"
-
 	dto "github.com/prometheus/client_model/go"
 	"github.com/gogo/protobuf/proto"
 	"sync"
 	"compress/gzip"
+	"math"
+	"strconv"
+	"bytes"
+	"github.com/alipay/sofa-mosn/pkg/buffer"
 )
 
 var (
@@ -46,6 +48,12 @@ var (
 	gzipPool        = sync.Pool{
 		New: func() interface{} {
 			return gzip.NewWriter(nil)
+		},
+	}
+	numBufPool = sync.Pool{
+		New: func() interface{} {
+			b := make([]byte, 0, 24)
+			return &b
 		},
 	}
 )
@@ -88,12 +96,12 @@ func (exporter *promHttpExporter) ServeHTTP(rsp http.ResponseWriter, req *http.R
 
 // ~ MetricsSink
 func (sink *promSink) Flush(writer io.Writer, ms []types.Metrics) {
-	format := expfmt.FmtText
+	//format := expfmt.FmtText
 	w := writer
 
 	rsp, ok := writer.(http.ResponseWriter)
 	if ok {
-		format = expfmt.Format(rsp.Header().Get("Content-Type"))
+		//format = expfmt.Format(rsp.Header().Get("Content-Type"))
 
 		// gzip
 		if rsp.Header().Get("Content-Encoding") == "gzip" {
@@ -106,116 +114,170 @@ func (sink *promSink) Flush(writer io.Writer, ms []types.Metrics) {
 			w = gz
 		}
 	}
-
-	enc := expfmt.NewEncoder(w, format)
-	familyMap := make(map[string]*dto.MetricFamily)
+	//enc := expfmt.NewEncoder(w, format)
+	//familyMap := make(map[string]*dto.MetricFamily)
 
 	// collect into MetricFamily
+	tracker := make(map[string]bool)
+
 	for _, m := range ms {
 		typ := m.Type()
 		labelKeys, labelVals := m.SortedLabels()
 
 		// TODO cached in metrics struct, avoid calc for each flush
 		prefix := strings.Join(labelKeys, "_") + "_" + typ + "_"
-		labels := makeLabelPair(labelKeys, labelVals)
+		suffix := makeLabelStr(labelKeys, labelVals)
+		//labels := makeLabelPair(labelKeys, labelVals)
 
 		m.Each(func(name string, i interface{}) {
+			buf := buffer.GetIoBuffer(128)
+
 			switch metric := i.(type) {
 			case gometrics.Counter:
-				fqName := prefix + name
-				family, ok := familyMap[fqName]
-				if !ok {
-					family = &dto.MetricFamily{
-						Name: proto.String(fqName),
-						Type: dto.MetricType_COUNTER.Enum(),
-					}
-					familyMap[fqName] = family
-				}
-				family.Metric = append(family.Metric,
-					&dto.Metric{
-						Label:   labels,
-						Counter: &dto.Counter{Value: proto.Float64(float64(metric.Count()))},
-					})
+				sink.flushCounter(tracker, buf, prefix+name, suffix, float64(metric.Count()))
+
+				//fqName := prefix + name
+				//family, ok := familyMap[fqName]
+				//if !ok {
+				//	family = &dto.MetricFamily{
+				//		Name: proto.String(fqName),
+				//		Type: dto.MetricType_COUNTER.Enum(),
+				//	}
+				//	familyMap[fqName] = family
+				//}
+				//family.Metric = append(family.Metric,
+				//	&dto.Metric{
+				//		Label:   labels,
+				//		Counter: &dto.Counter{Value: proto.Float64(float64(metric.Count()))},
+				//	})
 			case gometrics.Gauge:
-				fqName := prefix + name
-				family, ok := familyMap[fqName]
-				if !ok {
-					family = &dto.MetricFamily{
-						Name: proto.String(fqName),
-						Type: dto.MetricType_GAUGE.Enum(),
-					}
-					familyMap[fqName] = family
-				}
-				family.Metric = append(family.Metric,
-					&dto.Metric{
-						Label: labels,
-						Gauge: &dto.Gauge{Value: proto.Float64(float64(metric.Value()))},
-					})
+				sink.flushGauge(tracker, buf, prefix+name, suffix, float64(metric.Value()))
+
+				//fqName := prefix + name
+				//family, ok := familyMap[fqName]
+				//if !ok {
+				//	family = &dto.MetricFamily{
+				//		Name: proto.String(fqName),
+				//		Type: dto.MetricType_GAUGE.Enum(),
+				//	}
+				//	familyMap[fqName] = family
+				//}
+				//family.Metric = append(family.Metric,
+				//	&dto.Metric{
+				//		Label: labels,
+				//		Gauge: &dto.Gauge{Value: proto.Float64(float64(metric.Value()))},
+				//	})
 			case gometrics.Histogram:
-				snapshot := metric.Snapshot()
+				sink.flushHistogram(tracker, buf, prefix+name, suffix, metric.Snapshot())
 
-				// min
-				minFqName := prefix + name + "_min"
-				family, ok := familyMap[minFqName]
-				if !ok {
-					family = &dto.MetricFamily{
-						Name: proto.String(minFqName),
-						Type: dto.MetricType_GAUGE.Enum(),
-					}
-					familyMap[minFqName] = family
-				}
-				family.Metric = append(family.Metric,
-					&dto.Metric{
-						Label: labels,
-						Gauge: &dto.Gauge{Value: proto.Float64(float64(snapshot.Min()))},
-					})
-
-				// max
-				maxFqName := prefix + name + "_max"
-				family, ok = familyMap[maxFqName]
-				if !ok {
-					family = &dto.MetricFamily{
-						Name: proto.String(maxFqName),
-						Type: dto.MetricType_GAUGE.Enum(),
-					}
-					familyMap[maxFqName] = family
-				}
-				family.Metric = append(family.Metric,
-					&dto.Metric{
-						Label: labels,
-						Gauge: &dto.Gauge{Value: proto.Float64(float64(snapshot.Max()))},
-					})
+				//snapshot := metric.Snapshot()
+				//
+				//// min
+				//minFqName := prefix + name + "_min"
+				//family, ok := familyMap[minFqName]
+				//if !ok {
+				//	family = &dto.MetricFamily{
+				//		Name: proto.String(minFqName),
+				//		Type: dto.MetricType_GAUGE.Enum(),
+				//	}
+				//	familyMap[minFqName] = family
+				//}
+				//family.Metric = append(family.Metric,
+				//	&dto.Metric{
+				//		Label: labels,
+				//		Gauge: &dto.Gauge{Value: proto.Float64(float64(snapshot.Min()))},
+				//	})
+				//
+				//// max
+				//maxFqName := prefix + name + "_max"
+				//family, ok = familyMap[maxFqName]
+				//if !ok {
+				//	family = &dto.MetricFamily{
+				//		Name: proto.String(maxFqName),
+				//		Type: dto.MetricType_GAUGE.Enum(),
+				//	}
+				//	familyMap[maxFqName] = family
+				//}
+				//family.Metric = append(family.Metric,
+				//	&dto.Metric{
+				//		Label: labels,
+				//		Gauge: &dto.Gauge{Value: proto.Float64(float64(snapshot.Max()))},
+				//	})
 			}
+			buf.WriteTo(w)
+			buffer.PutIoBuffer(buf)
 		})
 	}
 
-	// encode
-	for _, family := range familyMap {
-		enc.Encode(family)
-	}
+	////encode
+	//for _, family := range familyMap {
+	//	enc.Encode(family)
+	//}
 }
 
-func (sink *promSink) flushHistogram(enc expfmt.Encoder, name string, labels []*dto.LabelPair, snapshot gometrics.Histogram) {
+//func (sink *promSink) flushHistogram(enc expfmt.Encoder, name string, labels []*dto.LabelPair, snapshot gometrics.Histogram) {
+//	// min
+//	sink.flushGauge(enc, name+"_min", labels, float64(snapshot.Min()))
+//	// max
+//	sink.flushGauge(enc, name+"_max", labels, float64(snapshot.Max()))
+//}
+//
+//func (sink *promSink) flushGauge(enc expfmt.Encoder, name string, labels []*dto.LabelPair, val float64) {
+//	enc.Encode(&dto.MetricFamily{
+//		Name:   proto.String(name),
+//		Type:   dto.MetricType_GAUGE.Enum(),
+//		Metric: []*dto.Metric{{Label: labels, Gauge: &dto.Gauge{Value: proto.Float64(val)}}},
+//	})
+//}
+//
+//func (sink *promSink) flushCounter(enc expfmt.Encoder, name string, labels []*dto.LabelPair, val float64) {
+//	enc.Encode(&dto.MetricFamily{
+//		Name:   proto.String(name),
+//		Type:   dto.MetricType_COUNTER.Enum(),
+//		Metric: []*dto.Metric{{Label: labels, Counter: &dto.Counter{Value: proto.Float64(val)}}},
+//	})
+//}
+
+func (sink *promSink) flushHistogram(tracker map[string]bool, buf types.IoBuffer, name string, labels string, snapshot gometrics.Histogram) {
 	// min
-	sink.flushGauge(enc, name+"_min", labels, float64(snapshot.Min()))
+	sink.flushGauge(tracker, buf, name+"_min", labels, float64(snapshot.Min()))
 	// max
-	sink.flushGauge(enc, name+"_max", labels, float64(snapshot.Max()))
+	sink.flushGauge(tracker, buf, name+"_max", labels, float64(snapshot.Max()))
 }
 
-func (sink *promSink) flushGauge(enc expfmt.Encoder, name string, labels []*dto.LabelPair, val float64) {
-	enc.Encode(&dto.MetricFamily{
-		Name:   proto.String(name),
-		Type:   dto.MetricType_GAUGE.Enum(),
-		Metric: []*dto.Metric{{Label: labels, Gauge: &dto.Gauge{Value: proto.Float64(val)}}},
-	})
+func (sink *promSink) flushGauge(tracker map[string]bool, buf types.IoBuffer, name string, labels string, val float64) {
+	// type
+	if !tracker[name] {
+		buf.WriteString("# TYPE ")
+		buf.WriteString(name)
+		buf.WriteString(" gauge\n")
+		tracker[name] = true
+	}
+	// metric
+	buf.WriteString(name)
+	buf.WriteString("{")
+	buf.WriteString(labels)
+	buf.WriteString("} ")
+	writeFloat(buf, val)
+	buf.WriteString("\n")
 }
 
-func (sink *promSink) flushCounter(enc expfmt.Encoder, name string, labels []*dto.LabelPair, val float64) {
-	enc.Encode(&dto.MetricFamily{
-		Name:   proto.String(name),
-		Type:   dto.MetricType_COUNTER.Enum(),
-		Metric: []*dto.Metric{{Label: labels, Counter: &dto.Counter{Value: proto.Float64(val)}}},
-	})
+func (sink *promSink) flushCounter(tracker map[string]bool, buf types.IoBuffer, name string, labels string, val float64) {
+	// type
+	if !tracker[name] {
+		buf.WriteString("# TYPE ")
+		buf.WriteString(name)
+		buf.WriteString(" counter\n")
+		tracker[name] = true
+	}
+
+	// metric
+	buf.WriteString(name)
+	buf.WriteString("{")
+	buf.WriteString(labels)
+	buf.WriteString("} ")
+	writeFloat(buf, val)
+	buf.WriteString("\n")
 }
 
 // NewPromeSink returns a metrics sink that produces Prometheus metrics using store data
@@ -283,6 +345,18 @@ func builder(cfg map[string]interface{}) (types.MetricsSink, error) {
 	return NewPromeSink(promCfg), nil
 }
 
+// input: keys=[cluster,host] values=[app1,server2]
+// output: cluster="app1",host="server"
+func makeLabelStr(keys, values []string) (out string) {
+	if length := len(keys); length > 0 {
+		out = keys[0] + "=\"" + values[0] + "\""
+		for i := 1; i < length; i++ {
+			out += "," + keys[i] + "=\"" + values[i] + "\""
+		}
+	}
+	return
+}
+
 func makeLabelPair(keys, values []string) (pairs []*dto.LabelPair) {
 	if length := len(keys); length == len(values) {
 		pairs = make([]*dto.LabelPair, length)
@@ -294,4 +368,33 @@ func makeLabelPair(keys, values []string) (pairs []*dto.LabelPair) {
 		}
 	}
 	return
+}
+
+func writeFloat(w types.IoBuffer, f float64) (int, error) {
+	switch {
+	case f == 1:
+		return w.WriteString("1.0")
+	case f == 0:
+		return w.WriteString("0.0")
+	case f == -1:
+		return w.WriteString("-1.0")
+	case math.IsNaN(f):
+		return w.WriteString("NaN")
+	case math.IsInf(f, +1):
+		return w.WriteString("+Inf")
+	case math.IsInf(f, -1):
+		return w.WriteString("-Inf")
+	default:
+		bp := numBufPool.Get().(*[]byte)
+		*bp = strconv.AppendFloat((*bp)[:0], f, 'g', -1, 64)
+		// Add a .0 if used fixed point and there is no decimal
+		// point already. This is for future proofing with OpenMetrics,
+		// where floats always contain either an exponent or decimal.
+		if !bytes.ContainsAny(*bp, "e.") {
+			*bp = append(*bp, '.', '0')
+		}
+		written, err := w.Write(*bp)
+		numBufPool.Put(bp)
+		return written, err
+	}
 }
--- pkg/metrics/sink/prometheus/prometheus_test.go
+++ pkg/metrics/sink/prometheus/prometheus_test.go
@@ -29,7 +29,8 @@ import (
 	"github.com/alipay/sofa-mosn/pkg/admin/store"
 	"github.com/alipay/sofa-mosn/pkg/metrics"
 	"fmt"
-	"os"
+	"net/url"
+	"io"
 )
 
 type testAction int
@@ -89,15 +90,12 @@ func TestPrometheusMetrics(t *testing.T) {
 	wg.Wait()
 
 	sink := NewPromeSink(&promConfig{
-		Port:                  8088,
-		Endpoint:              "/metrics",
+		Port:     8088,
+		Endpoint: "/metrics",
 		//DisableCollectProcess: true,
 		//DisableCollectGo:      true,
 	})
 	_ = sink
-
-
-	sink.Flush(os.Stdout, metrics.GetAll())
 
 	store.StartService(nil)
 	defer store.StopService()
@@ -119,7 +117,7 @@ func TestPrometheusMetrics(t *testing.T) {
 
 	body, _ := ioutil.ReadAll(resp.Body)
 
-	//fmt.Println(string(body))
+	fmt.Println(string(body))
 
 	if !bytes.Contains(body, []byte("lbk1_t1_k1{lbk1=\"lbv1\"} 0.0")) {
 		t.Error("lbk1_t1_k1{lbk1=\"lbv1\"} metric not correct")
@@ -163,14 +161,23 @@ func BenchmarkPromSink_Flush(b *testing.B) {
 	defer store.StopService()
 	time.Sleep(time.Second) // wait server start
 
-	//tc := http.Client{}
+	tc := http.Client{}
 	b.ResetTimer()
 	for i := 0; i < b.N; i ++ {
-		sink.Flush(ioutil.Discard, metrics.GetAll())
-		//resp, err := tc.Get("http://127.0.0.1:8088/metrics")
-		//if err != nil {
-		//	b.Error("get metrics failed:", err)
-		//}
-		//io.Copy(ioutil.Discard, resp.Body)
+		//sink.Flush(ioutil.Discard, metrics.GetAll())
+		url, _ := url.Parse("http://127.0.0.1:8088/metrics")
+		tc.Do(&http.Request{
+			Method: http.MethodGet,
+			URL:    url,
+			//Header: map[string][]string{
+			//	//"Accept-Encoding": {"gzip, deflate"},
+			//	"Accept": {"application/vnd.google.protobuf; proto=io.prometheus.client.MetricFamily encoding=compact-text"},
+			//},
+		})
+		resp, err := tc.Get("http://127.0.0.1:8088/metrics")
+		if err != nil {
+			b.Error("get metrics failed:", err)
+		}
+		io.Copy(ioutil.Discard, resp.Body)
 	}
 }
