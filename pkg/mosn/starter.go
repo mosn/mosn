dcfe2cbec2ee29a7f34efe9878511ab55d38b850
zonghai.szh <zonghai.szh@alibaba-inc.com>
2019-09-17 17:49:25 +0800

    fix log nil error.

--- pkg/mosn/starter.go
+++ pkg/mosn/starter.go
@@ -287,7 +287,7 @@ func (m *Mosn) Close() {
 // step1. NewMosn
 // step2. Start Mosn
 func Start(c *config.MOSNConfig) {
-	log.StartLogger.Infof("[mosn] [start] start by config : %+v", c)
+	log.DefaultLogger.Infof("[mosn] [start] start by config : %v", c)
 	Mosn := NewMosn(c)
 	Mosn.Start()
 	Mosn.wg.Wait()
