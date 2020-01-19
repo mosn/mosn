59f36395dcd11d989bc50c0f680ee40521288fa7
zonghaishang <yiji@apache.org>
2020-01-19 11:43:29 +0800

    fix failed to retry connection for x-protocol.

--- pkg/stream/xprotocol/connpool.go
+++ pkg/stream/xprotocol/connpool.go
@@ -218,12 +218,18 @@ func newActiveClient(ctx context.Context, pool *connPool) *activeClient {
 
 	log.DefaultLogger.Tracef("xprotocol new active client , try to create connection")
 	data := pool.host.CreateConnection(ctx)
-	data.Connection.Connect()
+
+	err := data.Connection.Connect()
+	/**
+	 *	fix unable to retry connection.
+	 */
+	if err != nil {
+		return nil
+	}
+
 	log.DefaultLogger.Tracef("xprotocol new active client , connect success %v", data)
 
 	log.DefaultLogger.Tracef("xprotocol new active client , try to create codec client")
-
-
 
 	connCtx := mosnctx.WithValue(context.Background(), types.ContextKeyConnectionID, data.Connection.ID())
 	connCtx = mosnctx.WithValue(connCtx, types.ContextSubProtocol, mosnctx.Get(ctx, types.ContextSubProtocol))
