ea71e39fefc91ccdc56627bb3f0f2cff9e9f870b
leizhiyuan <leizhiyuan@gmail.com>
2020-02-10 21:11:34 +0800

    fix:aliway read app name

--- pkg/trace/sofa/rpc/ext/boltv1.go
+++ pkg/trace/sofa/rpc/ext/boltv1.go
@@ -69,10 +69,7 @@ func boltv1Delegate(ctx context.Context, cmd sofarpc.SofaRpcCmd, span types.Span
 		}
 	}
 	span.SetTag(rpc.SPAN_ID, spanId)
-
-	if lType == v2.EGRESS {
-		span.SetTag(rpc.APP_NAME, request.RequestHeader[models.APP_NAME])
-	}
+	span.SetTag(rpc.APP_NAME, request.RequestHeader[models.APP_NAME])
 	span.SetTag(rpc.SPAN_TYPE, string(lType.(v2.ListenerType)))
 	span.SetTag(rpc.METHOD_NAME, request.RequestHeader[models.TARGET_METHOD])
 	span.SetTag(rpc.PROTOCOL, "bolt")
