77c3202277824bb9e3fb1d9a72bf3fcd6015477c
止语 <zhiyu.zxh@antfin.com>
2020-05-08 22:04:53 +0800

    fix: test code fix

--- pkg/filter/stream/flowcontrol/stream_filter_test.go
+++ pkg/filter/stream/flowcontrol/stream_filter_test.go
@@ -52,11 +52,11 @@ func TestStreamFilter(t *testing.T) {
 	m := make(map[string]string)
 	m["http_request_path"] = "/http"
 	m["dubbo_request_path"] = "/dubbo"
-	for k, v := range m {
+	for k := range m {
 		// register test variable
 		variable.RegisterVariable(variable.NewBasicVariable(k, nil,
 			func(ctx context.Context, variableValue *variable.IndexedValue, data interface{}) (s string, err error) {
-				return v, nil
+				return m[k], nil
 			}, nil, 0))
 	}
 	variable.RegisterProtocolResource(HTTP1, api.PATH, "http_request_path")
