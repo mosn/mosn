6009738f294c6fa3203a7448c881d70204d0440b
trainyao <trainyao@gmail.com>
2020-05-25 08:11:39 +0800

    mson.io/api maglev lb adjust

--- vendor/mosn.io/api/route.go
+++ vendor/mosn.io/api/route.go
@@ -126,6 +126,10 @@ type MetadataMatchCriteria interface {
 	MergeMatchCriteria(metadataMatches map[string]interface{}) MetadataMatchCriteria
 }
 
+type ConsistentHashCriteria interface {
+	HashType() ConsistentHashType
+}
+
 // PathMatchType defines the match pattern
 type PathMatchType uint32
 
@@ -136,6 +140,13 @@ const (
 	Exact
 	Regex
 	SofaHeader
+)
+
+// ConsistentHashType defines the consistent hash type
+type ConsistentHashType uint32
+
+const (
+	Maglev ConsistentHashType = iota
 )
 
 type PathMatchCriterion interface {
