800a740a80acc6e4c92b6ded49d9613af3138e10
trainyao <trainyao@gmail.com>
2020-06-07 14:52:06 +0800

    add deprecated const var

--- pkg/types/variable.go
+++ pkg/types/variable.go
@@ -62,4 +62,12 @@ const (
 const (
 	VarHttpRequestMethod = "http_request_method"
 	VarHttpRequestLength = "http_request_length"
+
+	// deprecated by adding protocol for prefix, use the const above instead
+	VarHttpRequestUri   = "http_request_uri"
+	VarHttpRequestPath  = "http_request_path"
+	VarHttpRequestArg   = "http_request_arg"
+	VarPrefixHttpHeader = "http_header_"
+	VarPrefixHttpArg    = "http_arg_"
+	VarPrefixHttpCookie = "http_cookie_"
 )
