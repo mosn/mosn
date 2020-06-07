e294f3df87796bf91ee0187063a2ad6743f108e7
champly <champly@outlook.com>
2020-06-07 08:52:12 +0800

    adapt pilot-agent liveness check

--- pkg/admin/server/istio.go
+++ pkg/admin/server/istio.go
@@ -13,15 +13,16 @@ import (
 )
 
 const (
-	CDS_UPDATE_SUCCESS = "cluster_manager.cds.update_success"
-	CDS_UPDATE_REJECT  = "cluster_manager.cds.update_rejected"
-	LDS_UPDATE_SUCCESS = "listener_manager.lds.update_success"
-	LDS_UPDATE_REJECT  = "listener_manager.lds.update_rejected"
-	SERVER_STATE       = "server.state"
+	CDS_UPDATE_SUCCESS   = "cluster_manager.cds.update_success"
+	CDS_UPDATE_REJECT    = "cluster_manager.cds.update_rejected"
+	LDS_UPDATE_SUCCESS   = "listener_manager.lds.update_success"
+	LDS_UPDATE_REJECT    = "listener_manager.lds.update_rejected"
+	SERVER_STATE         = "server.state"
+	STAT_WORKERS_STARTED = "listener_manager.workers_started"
 )
 
 func statsForIstio(w http.ResponseWriter, r *http.Request) {
-	state, err := getIstioState()
+	state, workersStarted, err := getIstioState()
 	if err != nil {
 		log.DefaultLogger.Warnf("%v", err)
 	}
@@ -32,6 +33,7 @@ func statsForIstio(w http.ResponseWriter, r *http.Request) {
 	sb.WriteString(fmt.Sprintf("%s: %d\n", LDS_UPDATE_SUCCESS, conv.Stats.LdsUpdateSuccess.Count()))
 	sb.WriteString(fmt.Sprintf("%s: %d\n", LDS_UPDATE_REJECT, conv.Stats.LdsUpdateReject.Count()))
 	sb.WriteString(fmt.Sprintf("%s: %d\n", SERVER_STATE, state))
+	sb.WriteString(fmt.Sprintf("%s: %d\n", STAT_WORKERS_STARTED, workersStarted))
 	_, err = sb.WriteTo(w)
 
 	if err != nil {
@@ -42,7 +44,7 @@ func statsForIstio(w http.ResponseWriter, r *http.Request) {
 func serverInfoForIstio(w http.ResponseWriter, r *http.Request) {
 	i := envoyControlPlaneAPI.ServerInfo{}
 	var err error
-	i.State, err = getIstioState()
+	i.State, _, err = getIstioState()
 	if err != nil {
 		log.DefaultLogger.Warnf("%v", err)
 		return
@@ -54,19 +56,21 @@ func serverInfoForIstio(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func getIstioState() (envoyControlPlaneAPI.ServerInfo_State, error) {
+func getIstioState() (envoyControlPlaneAPI.ServerInfo_State, int, error) {
 	mosnState := store.GetMosnState()
+	var workersStarted int
 
 	switch mosnState {
 	case store.Active_Reconfiguring:
-		return envoyControlPlaneAPI.ServerInfo_PRE_INITIALIZING, nil
+		return envoyControlPlaneAPI.ServerInfo_PRE_INITIALIZING, workersStarted, nil
 	case store.Init:
-		return envoyControlPlaneAPI.ServerInfo_INITIALIZING, nil
+		return envoyControlPlaneAPI.ServerInfo_INITIALIZING, workersStarted, nil
 	case store.Running:
-		return envoyControlPlaneAPI.ServerInfo_LIVE, nil
+		workersStarted = 1
+		return envoyControlPlaneAPI.ServerInfo_LIVE, workersStarted, nil
 	case store.Passive_Reconfiguring:
-		return envoyControlPlaneAPI.ServerInfo_DRAINING, nil
+		return envoyControlPlaneAPI.ServerInfo_DRAINING, workersStarted, nil
 	}
 
-	return 0, fmt.Errorf("parse mosn state %v to istio state failed", mosnState)
+	return 0, workersStarted, fmt.Errorf("parse mosn state %v to istio state failed", mosnState)
 }
