e96e2a9b435505d6d003c8a92cf5c461e41e8fcc
champly <champly@outlook.com>
2020-06-23 17:28:32 +0800

    complete metrics histogram percentiles config

--- pkg/metrics/sink/prometheus/prometheus.go
+++ pkg/metrics/sink/prometheus/prometheus.go
@@ -60,13 +60,14 @@ func init() {
 
 // promConfig contains config for all PromSink
 type promConfig struct {
-	ExportUrl string `json:"export_url"` // when this value is not nil, PromSink will work under the PUSHGATEWAY mode.
+	ExportUrl             string `json:"export_url"` // when this value is not nil, PromSink will work under the PUSHGATEWAY mode.
+	Port                  int    `json:"port"`       // pull mode attrs
+	Endpoint              string `json:"endpoint"`
+	DisableCollectProcess bool   `json:"disable_collect_process"`
+	DisableCollectGo      bool   `json:"disable_collect_go"`
+	Percentiles           []int  `json:"percentiles,omitempty"`
 
-	Port     int    `json:"port"` // pull mode attrs
-	Endpoint string `json:"endpoint"`
-
-	DisableCollectProcess bool `json:"disable_collect_process"`
-	DisableCollectGo      bool `json:"disable_collect_go"`
+	percentilesFloat []float64
 }
 
 // promSink extract metrics from stats registry with specified interval
@@ -118,7 +119,9 @@ func (psink *promSink) Flush(writer io.Writer, ms []types.Metrics) {
 			case gometrics.Gauge:
 				psink.flushGauge(tracker, buf, flattenKey(prefix+name), suffix, float64(metric.Value()))
 			case gometrics.Histogram:
-				psink.flushHistogram(tracker, buf, flattenKey(prefix+name), suffix, metric.Snapshot())
+				h := metric.Snapshot()
+				metric.Clear()
+				psink.flushHistogram(tracker, buf, flattenKey(prefix+name), suffix, h)
 			}
 			buf.WriteTo(w)
 			buf.Reset()
@@ -131,7 +134,16 @@ func (psink *promSink) flushHistogram(tracker map[string]bool, buf types.IoBuffe
 	psink.flushGauge(tracker, buf, name+"_min", labels, float64(snapshot.Min()))
 	// max
 	psink.flushGauge(tracker, buf, name+"_max", labels, float64(snapshot.Max()))
-	// TODO: flush P90 P95 P99 if configured
+	// flush P90 P95 P99 percentiles
+	if len(psink.config.Percentiles) == 0 {
+		return
+	}
+	ps := snapshot.Percentiles(psink.config.percentilesFloat)
+	if len(ps) == len(psink.config.Percentiles) {
+		for i, p := range psink.config.Percentiles {
+			psink.flushGauge(tracker, buf, name, fmt.Sprintf("%s,percentile=\"P%d\"", labels, p), ps[i])
+		}
+	}
 }
 
 func (psink *promSink) flushGauge(tracker map[string]bool, buf types.IoBuffer, name string, labels string, val float64) {
@@ -231,6 +243,14 @@ func builder(cfg map[string]interface{}) (types.MetricsSink, error) {
 		if !strings.HasPrefix(promCfg.Endpoint, "/") {
 			return nil, fmt.Errorf("invalid endpoint format:%s", promCfg.Endpoint)
 		}
+	}
+
+	if len(promCfg.Percentiles) > 0 {
+		percentilesFloat := make([]float64, 0, len(promCfg.Percentiles))
+		for _, p := range promCfg.Percentiles {
+			percentilesFloat = append(percentilesFloat, float64(p)/100)
+		}
+		promCfg.percentilesFloat = percentilesFloat
 	}
 
 	return NewPromeSink(promCfg), nil
