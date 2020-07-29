95f87c4caf5e62afadc3dc6bf4f6778ae2a5c841
champly <champly@outlook.com>
2020-07-29 16:59:40 +0800

    add unit test

--- pkg/metrics/sink/prometheus/prometheus_test.go
+++ pkg/metrics/sink/prometheus/prometheus_test.go
@@ -93,10 +93,8 @@ func TestPrometheusMetrics(t *testing.T) {
 	wg.Wait()
 
 	sink := NewPromeSink(&promConfig{
-		Port:             8088,
-		Endpoint:         "/metrics",
-		Percentiles:      []int{50, 90, 95, 99},
-		percentilesFloat: []float64{50, 90, 95, 99},
+		Port:     8088,
+		Endpoint: "/metrics",
 		//DisableCollectProcess: true,
 		//DisableCollectGo:      true,
 	})
@@ -146,6 +144,85 @@ func TestPrometheusMetrics(t *testing.T) {
 	if !bytes.Contains(body, []byte("t1_k4_min{lbk2=\"lbv2\"} 2.0")) {
 		t.Error("t1_k4_min{lbk2=\"lbv2\"} metric not correct")
 	}
+}
+
+func TestPrometheusHistogramMetrics(t *testing.T) {
+	metrics.ResetAll()
+	testCases := []struct {
+		typ         string
+		labels      map[string]string
+		key         string
+		action      testAction
+		actionValue int64
+	}{
+		{"t1", map[string]string{"lbk1": "lbv1"}, "k1", countInc, 1},
+		{"t1", map[string]string{"lbk1": "lbv1", "lbk2": "lbv2"}, "k1", countInc, 1},
+		{"t1", map[string]string{"lbk1": "lbv2"}, "k1", countInc, 1},
+		{"t1", map[string]string{"lbk1": "lbv1"}, "k1", countDec, 1},
+		{"t1", map[string]string{"lbk1": "lbv1"}, "k2", countInc, 1},
+		{"t1", map[string]string{"lbk1": "lbv1"}, "k3", gaugeUpdate, 1},
+		{"t1", map[string]string{"lbk1": "lbv1"}, "k4", histogramUpdate, 1},
+		{"t1", map[string]string{"lbk1": "lbv1"}, "k4", histogramUpdate, 2},
+		{"t1", map[string]string{"lbk1": "lbv1"}, "k4", histogramUpdate, 3},
+		{"t1", map[string]string{"lbk1": "lbv1"}, "k4", histogramUpdate, 4},
+		{"t1", map[string]string{"lbk2": "lbv2"}, "k1", countInc, 1},
+		{"t1", map[string]string{"lbk2": "lbv2"}, "k2", countInc, 2},
+		{"t1", map[string]string{"lbk2": "lbv2"}, "k3", gaugeUpdate, 3},
+		{"t1", map[string]string{"lbk2": "lbv2"}, "k4", histogramUpdate, 2},
+		{"t2", map[string]string{"lbk1": "lbv1"}, "k1", countInc, 1},
+	}
+	wg := sync.WaitGroup{}
+	for i := range testCases {
+		wg.Add(1)
+		go func(i int) {
+			tc := testCases[i]
+			s, _ := metrics.NewMetrics(tc.typ, tc.labels)
+			switch tc.action {
+			case countInc:
+				s.Counter(tc.key).Inc(tc.actionValue)
+			case countDec:
+				s.Counter(tc.key).Dec(tc.actionValue)
+			case gaugeUpdate:
+				s.Gauge(tc.key).Update(tc.actionValue)
+			case histogramUpdate:
+				s.Histogram(tc.key).Update(tc.actionValue)
+			}
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+
+	sink, _ := builder(map[string]interface{}{
+		"port":        8088,
+		"endpoint":    "/metrics",
+		"percentiles": []int{50, 90, 95, 99},
+	})
+	_ = sink
+
+	store.StartService(nil)
+	defer func() {
+		// stop service is running as a goroutine
+		// we sleep a second to make sure stop service finished
+		store.StopService()
+		time.Sleep(time.Second)
+	}()
+	time.Sleep(time.Second) // wait server start
+
+	tc := http.Client{}
+
+	resp, err := tc.Get("http://127.0.0.1:8088/metrics")
+	if err != nil {
+		// wait listener ready
+		time.Sleep(time.Second)
+		resp, err = tc.Get("http://127.0.0.1:8088/metrics")
+
+		// still error
+		if err != nil {
+			t.Error("get metrics failed:", err)
+		}
+	}
+
+	body, _ := ioutil.ReadAll(resp.Body)
 
 	if !bytes.Contains(body, []byte(`t1_k4{lbk2="lbv2",percentile="P50"} 2.0`)) {
 		t.Error(`t1_k4{lbk2="lbv2",percentile="P50"} metric not correct`)
