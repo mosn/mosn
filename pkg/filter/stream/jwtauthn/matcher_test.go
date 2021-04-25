b3b875c7c0436a7f30d2498138e782ad6d450724
黄润豪 <chaoschunqfeijg@163.com>
2021-04-25 15:18:14 +0800

    feature: URL prefix case-sensitive (#1637)
    * bug fix: URL prefix case-sensitive
    
    * default case sensitive
    
    * eliminate repeated judgments

--- pkg/filter/stream/jwtauthn/matcher_test.go
+++ pkg/filter/stream/jwtauthn/matcher_test.go
@@ -6,7 +6,6 @@ import (
 	routev3 "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
 	jwtauthnv3 "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/jwt_authn/v3"
 	"github.com/golang/protobuf/ptypes/wrappers"
-	//matcherv3 "github.com/envoyproxy/go-control-plane/envoy/type/matcher/v3"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -25,6 +24,21 @@ func TestMatchPrefix(t *testing.T) {
 	assert.True(t, matcher.Matches(nil, "/matching"))
 	assert.False(t, matcher.Matches(nil, "/matc"))
 	assert.False(t, matcher.Matches(nil, "/no"))
+}
+
+func TestMatchPrefixCaseInsensitive(t *testing.T) {
+	rule := &jwtauthnv3.RequirementRule{
+		Match: &routev3.RouteMatch{
+			PathSpecifier: &routev3.RouteMatch_Prefix{
+				Prefix: "/match",
+			},
+			CaseSensitive: &wrappers.BoolValue{Value: false},
+		},
+	}
+
+	matcher := NewMatcher(rule)
+	assert.True(t, matcher.Matches(nil, "/matching"))
+	assert.True(t, matcher.Matches(nil, "/MATCH"))
 }
 
 func TestMatchPath(t *testing.T) {
