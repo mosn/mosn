ea70d69c1031da6101b14bbe05ee087c15471e24
“yichouchou” <759960474@qq.com>
2021-05-24 11:05:51 +0800

    将日志关闭的函数挪到signalCallback

--- pkg/server/keeper/serverkeeper.go
+++ pkg/server/keeper/serverkeeper.go
@@ -116,6 +116,9 @@ func catchSignalsCrossPlatform() {
 }
 
 func executeSignalCallback(sig syscall.Signal) {
+	AddSignalCallback(func() {
+		log.CloseAll()
+	}, syscall.SIGTERM)
 	if cbs, ok := signalCallback[sig]; ok {
 		for _, cb := range cbs {
 			cb()
