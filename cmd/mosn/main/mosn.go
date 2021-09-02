7a7c04d74b585da882335e204761f5375f273325
文徐 <wangwenxue.wwx@alibaba-inc.com>
2021-09-02 16:13:53 +0800

    fix compile error

--- cmd/mosn/main/mosn.go
+++ cmd/mosn/main/mosn.go
@@ -35,7 +35,7 @@ import (
 	_ "mosn.io/mosn/pkg/filter/stream/faultinject"
 	_ "mosn.io/mosn/pkg/filter/stream/faulttolerance"
 	_ "mosn.io/mosn/pkg/filter/stream/flowcontrol"
-	_ "mosn.io/mosn/pkg/filter/stream/grpc"
+	_ "mosn.io/mosn/pkg/filter/stream/grpcmetric"
 	_ "mosn.io/mosn/pkg/filter/stream/gzip"
 	_ "mosn.io/mosn/pkg/filter/stream/headertometadata"
 	_ "mosn.io/mosn/pkg/filter/stream/jwtauthn"
