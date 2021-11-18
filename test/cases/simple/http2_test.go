864054211309167c759537a1f25942f603bcd90a
taoyuanyuan <linuxty@gmail.com>
2021-11-18 21:23:09 +0800

    dd

--- test/cases/simple/http2_test.go
+++ test/cases/simple/http2_test.go
@@ -362,7 +362,6 @@ const ConfigSimpleHTTP2UseStream = `{
                 {
                         "default_log_path":"stdout",
                         "default_log_level": "DEBUG",
-
                         "routers": [
                                 {
                                         "router_config_name":"router_to_server",
