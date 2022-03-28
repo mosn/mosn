874a7160d1d5c0248e1bf3ee4a83997d4de0f135
z2z23n0 <zhangyuzezzz@gmail.com>
2022-03-28 20:33:34 +0800

    improvement: simplify code

--- pkg/stream/http2/stream.go
+++ pkg/stream/http2/stream.go
@@ -178,11 +178,12 @@ func streamConfigHandler(v interface{}) interface{} {
 func parseStreamConfig(ctx context.Context) StreamConfig {
 	streamConfig := defaultStreamConfig
 	// get extend config from ctx
-	pgc := mosnctx.Get(ctx, types.ContextKeyProxyGeneralConfig)
-	if extendConfig, ok := pgc.(map[api.ProtocolName]interface{}); ok {
-		if http2Config, ok := extendConfig[protocol.HTTP2]; ok {
-			if cfg, ok := http2Config.(StreamConfig); ok {
-				streamConfig = cfg
+	if pgc := mosnctx.Get(ctx, types.ContextKeyProxyGeneralConfig); pgc != nil {
+		if extendConfig, ok := pgc.(map[api.ProtocolName]interface{}); ok {
+			if http2Config, ok := extendConfig[protocol.HTTP2]; ok {
+				if cfg, ok := http2Config.(StreamConfig); ok {
+					streamConfig = cfg
+				}
 			}
 		}
 	}
@@ -221,15 +222,7 @@ func newServerStreamConnection(ctx context.Context, connection api.Connection, s
 		serverCallbacks: serverCallbacks,
 	}
 
-	if pgc := mosnctx.Get(ctx, types.ContextKeyProxyGeneralConfig); pgc != nil {
-		if extendConfig, ok := pgc.(map[api.ProtocolName]interface{}); ok {
-			if http2Config, ok := extendConfig[protocol.HTTP2]; ok {
-				if cfg, ok := http2Config.(StreamConfig); ok {
-					sc.useStream = cfg.Http2UseStream
-				}
-			}
-		}
-	}
+	sc.useStream = sc.config.Http2UseStream
 
 	// init first context
 	sc.cm.Next()
@@ -663,15 +656,7 @@ func newClientStreamConnection(ctx context.Context, connection api.Connection,
 		streamConnectionEventListener: clientCallbacks,
 	}
 
-	if pgc := mosnctx.Get(ctx, types.ContextKeyProxyGeneralConfig); pgc != nil {
-		if extendConfig, ok := pgc.(map[api.ProtocolName]interface{}); ok {
-			if http2Config, ok := extendConfig[protocol.HTTP2]; ok {
-				if cfg, ok := http2Config.(StreamConfig); ok {
-					sc.useStream = cfg.Http2UseStream
-				}
-			}
-		}
-	}
+	sc.useStream = parseStreamConfig(ctx).Http2UseStream
 
 	// init first context
 	sc.cm.Next()
