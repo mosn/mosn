ee521cba49d3b1157ac5d263304faf6db2fa23f4
Junlong <fu4ng@foxmail.com>
2022-04-14 23:10:49 +0800

    chore(copy modifications to istio/main):

--- istio/_istio152/main/control.go
+++ istio/_istio152/main/control.go
@@ -177,8 +177,19 @@ var (
 	cmdStop = cli.Command{
 		Name:  "stop",
 		Usage: "stop mosn proxy",
-		Action: func(c *cli.Context) error {
-			return nil
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:   "config, c",
+				Usage:  "load configuration from `FILE`",
+				EnvVar: "MOSN_CONFIG",
+				Value:  "configs/mosn_config.json",
+			},
+		},
+		Action: func(c *cli.Context) (err error) {
+			app := mosn.NewMosn()
+			stm := stagemanager.InitStageManager(c, c.String("config"), app)
+			stm.AppendInitStage(mosn.InitDefaultPath)
+			return stm.StopMosnProcess()
 		},
 	}
 
