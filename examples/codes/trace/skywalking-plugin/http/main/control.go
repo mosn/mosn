a9106fee38f02464520c882777fbbef1fd068d0a
wanghongjia.whj <wanghongjia.whj@antgroup.com>
2022-06-07 09:40:46 +0800

    feat: fix

--- examples/codes/trace/skywalking-plugin/http/main/control.go
+++ examples/codes/trace/skywalking-plugin/http/main/control.go
@@ -27,7 +27,6 @@ import (
 
 	"github.com/urfave/cli"
 
-	"mosn.io/mosn/istio/istio1106"
 	v2 "mosn.io/mosn/pkg/config/v2"
 	"mosn.io/mosn/pkg/configmanager"
 	"mosn.io/mosn/pkg/featuregate"
@@ -163,29 +162,6 @@ var (
 			stm.AppendInitStage(func(cfg *v2.MOSNConfig) {
 				drainTime := c.Int("drain-time-s")
 				server.SetDrainTime(time.Duration(drainTime) * time.Second)
-				// istio parameters
-				serviceCluster := c.String("service-cluster")
-				serviceNode := c.String("service-node")
-				serviceType := c.String("service-type")
-				serviceMeta := c.StringSlice("service-meta")
-				metaLabels := c.StringSlice("service-lables")
-				clusterDomain := c.String("cluster-domain")
-				podName := c.String("pod-name")
-				podNamespace := c.String("pod-namespace")
-				podIp := c.String("pod-ip")
-
-				if serviceNode != "" {
-					istio1106.InitXdsInfo(cfg, serviceCluster, serviceNode, serviceMeta, metaLabels)
-				} else {
-					if istio1106.IsApplicationNodeType(serviceType) {
-						sn := podName + "." + podNamespace
-						serviceNode = serviceType + "~" + podIp + "~" + sn + "~" + clusterDomain
-						istio1106.InitXdsInfo(cfg, serviceCluster, serviceNode, serviceMeta, metaLabels)
-					} else {
-						log.StartLogger.Infof("[mosn] [start] xds service type is not router/sidecar, use config only")
-						istio1106.InitXdsInfo(cfg, "", "", nil, nil)
-					}
-				}
 			})
 			stm.AppendInitStage(mosn.DefaultInitStage)
 			stm.AppendInitStage(func(_ *v2.MOSNConfig) {
