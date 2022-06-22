19ac80db3a6addac288ecb197cae996b3d3c36a8
threestoneliu <threestoneliu@outlook.com>
2022-06-22 16:10:35 +0800

    fix typos
    Signed-off-by: threestoneliu <threestoneliu@outlook.com>

--- pkg/types/network.go
+++ pkg/types/network.go
@@ -201,7 +201,7 @@ var (
 type ConnectionHandler interface {
 	// AddOrUpdateListener
 	// adds a listener into the ConnectionHandler or
-	// update a listener
+	// updates a listener
 	AddOrUpdateListener(lc *v2.Listener) (ListenerEventListener, error)
 
 	//StartListeners starts all listeners the ConnectionHandler has
@@ -213,25 +213,25 @@ type ConnectionHandler interface {
 	// FindListenerByName finds and returns a listener by the listener name
 	FindListenerByName(name string) Listener
 
-	// RemoveListeners find and removes a listener by listener name.
+	// RemoveListeners finds and removes a listener by listener name.
 	RemoveListeners(name string)
 
-	// GracefulStopListener graceful stop a listener by listener name
+	// GracefulStopListener graceful stops a listener by listener name
 	// stop accept connections + graceful stop existing connections
 	GracefulStopListener(lctx context.Context, name string) error
 
-	// GracefulCloseListener graceful close a listener by listener name
+	// GracefulCloseListener graceful closes a listener by listener name
 	// stop accept connections + graceful stop existing connections + close listener
 	GracefulCloseListener(lctx context.Context, name string) error
 
-	// GracefulStopListeners stop accept connections from all listeners the ConnectionHandler has.
+	// GracefulStopListeners stops accept connections from all listeners the ConnectionHandler has.
 	// and graceful stop all the existing connections.
 	GracefulStopListeners() error
 
-	// CloseListeners close listeners immediately
+	// CloseListeners closes listeners immediately
 	CloseListeners()
 
-	// ListListenersFD reports all listeners' fd
+	// ListListenersFile reports all listeners' fd
 	ListListenersFile(lctx context.Context) []*os.File
 
 	// StopConnection Stop Connection
