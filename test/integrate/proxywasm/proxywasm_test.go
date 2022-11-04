3be2a2aa0391c48d30424d062e171b3741406819
Crypt Keeper <64215+codefromthecrypt@users.noreply.github.com>
2022-11-04 09:02:19 +0800

    Adds integrated benchmarks for proxywasam (#2164)
    This adds benchmarks for the only proxy-wasm compatible ABI available in
    mosn: v1. This is mainly an effort to help folks recognize the
    performance overhead and also how critical being incompatible is for the
    ecosystem.
    
    For example, the wasm in the below test only adds one header and nothing
    else. To do that takes over 60us. In comparison, other wasm handlers can
    do the same in 2-3us.
    ```
    Benchmark_BaseCase-16       	    8696	    133748 ns/op
    Benchmark_ProxyWasmV1-16    	    5760	    195353 ns/op
    ```
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

---
+++ test/integrate/proxywasm/proxywasm_test.go
@@ -0,0 +1,213 @@
+//go:build wasmer
+// +build wasmer
+
+package wasm_test
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"path/filepath"
+	"testing"
+	"time"
+
+	config "mosn.io/mosn/pkg/config/v2"
+	_ "mosn.io/mosn/pkg/filter/network/proxy"
+	_ "mosn.io/mosn/pkg/filter/stream/proxywasm"
+	_ "mosn.io/mosn/pkg/stream/http"
+	_ "mosn.io/mosn/pkg/stream/http2"
+	_ "mosn.io/mosn/pkg/wasm/runtime/wasmer"
+	"mosn.io/mosn/test/util/mosn"
+)
+
+type testMosn struct {
+	url     string
+	logPath string
+	*mosn.MosnWrapper
+}
+
+const pathResponseHeaderV1 = "testdata/req-header-v1/main.wasm"
+
+func Test_ProxyWasmV1(t *testing.T) {
+	backend := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("Wasm-Context") == "" {
+			t.Fatalf("expected to see request header from wasm: %v", r.Header)
+		}
+	}))
+	defer backend.Close()
+	logPath := filepath.Join(t.TempDir(), "mosn.log")
+
+	mosn, err := startMosn(backend.Listener.Addr().String(), pathResponseHeaderV1, logPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer mosn.Close()
+
+	resp, err := http.Get(mosn.url)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+}
+
+func Benchmark_BaseCase(b *testing.B) {
+	benchmark(b, "")
+}
+
+func Benchmark_ProxyWasmV1(b *testing.B) {
+	benchmark(b, pathResponseHeaderV1)
+}
+
+func benchmark(b *testing.B, wasmPath string) {
+	backend := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+	defer backend.Close()
+	logPath := filepath.Join(b.TempDir(), "mosn.log")
+
+	mosn, err := startMosn(backend.Listener.Addr().String(), wasmPath, logPath)
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer mosn.Close()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		resp, err := http.Get(mosn.url)
+		if err != nil {
+			b.Fatal(err)
+		}
+		resp.Body.Close()
+	}
+}
+
+func startMosn(backendAddr string, wasmPath, logPath string) (testMosn, error) {
+	port := freePort()
+	adminPort := freePort()
+	c := &config.MOSNConfig{
+		Servers: []config.ServerConfig{
+			{
+				DefaultLogPath:  logPath,
+				DefaultLogLevel: "ERROR",
+				Routers: []*config.RouterConfiguration{
+					{
+						RouterConfigurationConfig: config.RouterConfigurationConfig{
+							RouterConfigName: "server_router",
+						},
+						VirtualHosts: []config.VirtualHost{
+							{
+								Name:    "serverHost",
+								Domains: []string{"*"},
+								Routers: []config.Router{
+									{
+										RouterConfig: config.RouterConfig{
+											Match: config.RouterMatch{
+												Prefix: "/",
+											},
+											Route: config.RouteAction{
+												RouterActionConfig: config.RouterActionConfig{
+													ClusterName: "serverCluster",
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+				Listeners: []config.Listener{
+					{
+						ListenerConfig: config.ListenerConfig{
+							Name:       "serverListener",
+							AddrConfig: fmt.Sprintf("127.0.0.1:%d", port),
+							BindToPort: true,
+							FilterChains: []config.FilterChain{
+								{
+									FilterChainConfig: config.FilterChainConfig{
+										Filters: []config.Filter{
+											{
+												Type: "proxy",
+												Config: map[string]interface{}{
+													"downstream_protocol": "Http1",
+													"upstream_protocol":   "Http1",
+													"router_config_name":  "server_router",
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		ClusterManager: config.ClusterManagerConfig{
+			Clusters: []config.Cluster{
+				{
+					Name:                 "serverCluster",
+					ClusterType:          "SIMPLE",
+					LbType:               "LB_RANDOM",
+					MaxRequestPerConn:    1024,
+					ConnBufferLimitBytes: 32768,
+					Hosts: []config.Host{
+						{
+							HostConfig: config.HostConfig{
+								Address: backendAddr,
+							},
+						},
+					},
+				},
+			},
+		},
+		RawAdmin: &config.Admin{
+			Address: &config.AddressInfo{
+				SocketAddress: config.SocketAddress{
+					Address:   "127.0.0.1",
+					PortValue: uint32(adminPort),
+				},
+			},
+		},
+		DisableUpgrade: true,
+	}
+	if wasmPath != "" {
+		c.Servers[0].Listeners[0].ListenerConfig.StreamFilters = []config.Filter{
+			{
+				Type: "proxywasm",
+				Config: map[string]interface{}{
+					"instance_num": 1,
+					"vm_config": map[string]interface{}{
+						"engine": "wasmer",
+						"path":   wasmPath,
+					},
+				},
+			},
+		}
+	}
+	app := mosn.NewMosn(c)
+	app.Start()
+	for i := 0; i < 100; i++ {
+		time.Sleep(200 * time.Millisecond)
+		resp, err := http.Get(fmt.Sprintf("http://127.0.0.1:%d", adminPort))
+		if err != nil {
+			continue
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode == http.StatusOK {
+			time.Sleep(1 * time.Second)
+			return testMosn{
+				url:         fmt.Sprintf("http://127.0.0.1:%d", port),
+				logPath:     logPath,
+				MosnWrapper: app,
+			}, nil
+		}
+	}
+	return testMosn{}, errors.New("mosn start failed")
+}
+
+func freePort() int {
+	l, _ := net.Listen("tcp", ":0")
+	defer l.Close()
+	return l.Addr().(*net.TCPAddr).Port
+}
