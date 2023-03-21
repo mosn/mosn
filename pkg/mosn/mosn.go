0ad9a3cf25cd3ea04bb0a05682fa167c0b756e35
muyi.dq <muyi.dq@alibaba-inc.com>
2023-03-21 16:48:36 +0800

    add error log

--- pkg/mosn/mosn.go
+++ pkg/mosn/mosn.go
@@ -101,7 +101,7 @@ func (m *Mosn) inheritHandler() error {
 	var err error
 	m.Upgrade.InheritListeners, m.Upgrade.InheritPacketConn, m.Upgrade.ListenSockConn, err = server.GetInheritListeners()
 	if err != nil {
-		log.StartLogger.Errorf("[mosn] [NewMosn] getInheritListeners failed, exit")
+		log.StartLogger.Errorf("[mosn] [NewMosn] getInheritListeners failed, err:%v, exit", err)
 		return err
 	}
 	log.StartLogger.Infof("[mosn] [NewMosn] active reconfiguring")
