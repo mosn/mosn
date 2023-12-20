3df638cb119a93fb1a658107712d998269aca11a
AlphaBaby <fujianhao1997@qq.com>
2023-12-20 17:50:14 +0800

    fix: set max_header_size not effect, not recycle a bad conn for http1

--- pkg/stream/http/stream.go
+++ pkg/stream/http/stream.go
@@ -244,7 +244,8 @@ func newClientStreamConnection(ctx context.Context, connection types.ClientConne
 	if pgc, err := variable.Get(ctx, types.VariableProxyGeneralConfig); err == nil && pgc != nil {
 		if extendConfig, ok := pgc.(map[api.ProtocolName]interface{}); ok {
 			if http1Config, ok := extendConfig[protocol.HTTP1]; ok {
-				if config, ok := http1Config.(map[string]interface{}); ok {
+				switch config := http1Config.(type) {
+				case map[string]interface{}:
 					if v, ok := config["max_header_size"]; ok {
 						// json.Unmarshal stores float64 for JSON numbers in the interface{}
 						// see doc: https://golang.org/pkg/encoding/json/#Unmarshal
@@ -252,6 +253,8 @@ func newClientStreamConnection(ctx context.Context, connection types.ClientConne
 							maxResponseHeaderSize = int(fv)
 						}
 					}
+				case StreamConfig:
+					maxResponseHeaderSize = config.MaxHeaderSize
 				}
 			}
 		}
@@ -712,7 +715,8 @@ func (s *clientStream) endStream() {
 	err := s.doSend()
 
 	if err != nil {
-		log.Proxy.Errorf(s.stream.ctx, "[stream] [http] send client request error: %+v", err)
+		log.Proxy.Errorf(s.stream.ctx, "[stream] [http] send client request, connID %d, error: %+v",
+			s.connection.conn.ID(), err)
 
 		if err == types.ErrConnectionHasClosed {
 			s.ResetStream(types.StreamConnectionFailed)
@@ -723,7 +727,8 @@ func (s *clientStream) endStream() {
 	}
 
 	if log.Proxy.GetLogLevel() >= log.DEBUG {
-		log.Proxy.Debugf(s.stream.ctx, "[stream] [http] send client request, requestId = %v", s.stream.id)
+		log.Proxy.Debugf(s.stream.ctx, "[stream] [http] send client request, requestId = %v, connID %d",
+			s.stream.id, s.connection.conn.ID())
 	}
 	s.connection.requestSent <- true
 }
